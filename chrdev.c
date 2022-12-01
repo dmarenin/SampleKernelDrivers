210ccfe6a95a83bfe7706066933d32a0f4f947cd
dmarenin <39666107+dmarenin@users.noreply.github.com>
2022-12-01 23:52:08 +0500

    add counter ref

--- chrdev.c
+++ chrdev.c
@@ -5,7 +5,7 @@
 #include <linux/slab.h>
 #include <linux/cdev.h>
 
-static char *kbuf;
+//static char *kbuf; globalbuf
 static dev_t first;
 static unsigned int count = 1;
 static int my_major = 700, my_minor = 0;
@@ -16,18 +16,41 @@ static struct cdev *my_cdev;
 
 static int mychdev_open(struct inode *inode, struct file *file)
 {
+ static int counter = 0;
+ char* kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
+
+ file->private_data = kbuf;
+
  printk(KERN_INFO "Opening device %s: \n\n", MYDEV_NAME);
+
+ counter++;
+
+ printk(KERN_INFO "Counter  %d: \n", counter);
+ printk(KERN_INFO "Module ref counter d: \n", module_refcount(THIS_MODULE));
+
  return 0;
 }
 
 static int mychrdev_release(struct inode *inode, struct file *file)
 {
  printk(KERN_INFO "Releasing device %s: \n\n", MYDEV_NAME);
+
+ char* kbuf = file->private_data;
+
+ printk(KERN_INFO "Free buffer");
+ if (kbuf)
+  kfree(kbuf);
+
+ kbuf = NULL;
+ file->private_data = NULL;
+
  return 0;
 }
 
 static ssize_t mychrdev_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos)
 {
+ char *kbuf = file->private_data;
+
  int nbytes = lbuf - copy_to_user(buf, kbuf + *ppos, lbuf);
  *ppos += nbytes;
 
@@ -38,6 +61,8 @@ static ssize_t mychrdev_read(struct file *file, char __user *buf, size_t lbuf, l
 
 static ssize_t mychrdev_write(struct file *file, const char __user *buf, size_t lbuf, loff_t *ppos)
 {
+ char *kbuf = file->private_data;
+
  int nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
  *ppos += nbytes;
 
@@ -57,7 +82,7 @@ static int __init init_chrdev(void)
 {
  printk(KERN_INFO "Hello, loading");
 
- kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
+ //kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
 
  first = MKDEV(my_major, my_minor);
 
@@ -73,6 +98,12 @@ static int __init init_chrdev(void)
 static void __exit cleanup_chrdev(void)
 {
  printk(KERN_INFO "Leaving");
+
+ if (my_cdev)
+  cdev_del(my_cdev);
+ 
+ unregister_chrdev_region(first, count);
+
 }
 
 module_init(init_chrdev);
